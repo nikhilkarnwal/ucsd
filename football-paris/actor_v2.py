fe9a5d9ba97efe0c37e498d8375d72abd7bf6277
nikhilkarnwal <nikhilkarnwal@yahoo.com>
2021-03-23 10:27:59 +0530

    Added epi

--- football-paris/actor_v2.py
+++ football-paris/actor_v2.py
@@ -177,7 +177,7 @@ def actor_self(actor_num, center_model, data_queue, signal_queue, summary_queue,
 
     n_epi = 0
     rollout = []
-    while True:  # episode loop
+    while n_epi < arg_dict['n_epi']:  # episode loop
         opp_model_num, opp_model_path = select_opponent(arg_dict)
         checkpoint = torch.load(opp_model_path, map_location=cpu_device)
         opp_model.load_state_dict(checkpoint['model_state_dict'])
